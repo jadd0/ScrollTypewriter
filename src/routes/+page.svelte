f852c21b831ec5c71a770fe4e06d94918c0e760c
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-02-06 20:16:56 +0000

    code in place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,63 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let height;
+	export let furthestScrolled;
+
+	$: if (furthestScrolled >= height && !isTyping) {
+		typing();
+	}
+
+	export let phrase = ''; 
+	let typedChar = '';
+	let index = 0;
+	let typewriter; 
+
+	let isTyping = false;
+
+	const typeChar = () => {
+		if (index < phrase.length) {
+			isTyping = true;
+			typedChar += phrase[index];
+			index += 1;
+		} else {
+			return;
+		}
+	}
+
+	const typing = () => (typewriter = setInterval(typeChar, 20));
+</script>
+
+<main>
+	<section>
+		<p>{typedChar}</p>
+	</section>
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+	
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		width: 80%;
+		min-height: 50vh;
+		margin: 2% 0 1%;
+		padding: 20px;
+		box-shadow: 0 -8px 10px -6px #000000, 8px 0 10px -6px #000000;
+		overflow: scroll;
+	}
+
+	p {
+		font-size: 1.2rem;
+		line-height: 150%;
+    color: white;
+    text-align:left;
+	}
+</style>
